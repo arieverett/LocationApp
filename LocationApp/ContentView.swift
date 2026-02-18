3ae9bb02dfc918ab67bfd43ef1fe30ed5acc774c
ari <ariteverett@gmail.com>
2026-02-18 10:50:33 -0700

    Initial Commit

---
+++ LocationApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LocationApp
+//
+//  Created by Ari Everett on 2/18/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LocationApp/LocationAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocationAppApp.swift
+//  LocationApp
+//
+//  Created by Ari Everett on 2/18/26.
+//
+
+import SwiftUI
+
+@main
+struct LocationAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
